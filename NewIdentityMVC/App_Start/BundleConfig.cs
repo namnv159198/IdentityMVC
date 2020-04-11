cac14d9964ca2ced7998db91aa7a869e6c7b43e5
nam159198 <namkun159198@gmail.com>
2020-04-11 13:30:19 +0700

    Add datatable for list product

--- NewIdentityMVC/App_Start/BundleConfig.cs
+++ NewIdentityMVC/App_Start/BundleConfig.cs
@@ -9,7 +9,10 @@ namespace NewIdentityMVC
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/DataTables/jquery.dataTables.js",
+                        "~/Scripts/DataTables/dataTables.bootstrap.js",
+                        "~/Scripts//jquery-1.10.2.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -24,7 +27,8 @@ namespace NewIdentityMVC
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/DataTables/css/dataTables.bootstrap.css"));
         }
     }
 }
